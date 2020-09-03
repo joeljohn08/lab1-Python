7d47efefc5f03fa93dcb2e69d72f4d684fd091cf
JoelJohn08 <jmj6182@psu.edu>
2020-09-03 21:06:44 +0000

    lala

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ Temperature = input ("Enter Temperature:")
 Temperature = float(Temperature)
 unit = input ("Enter the Unit as F/f or C/c:")
 if unit == "F" or unit == "f":
- F = (Temperature-32) * 1.8
- print(f"{Temperature}°Celsius is {F}° Fahrenheit.")
+ C = Temperature*1.8 +32
+ print(f"{Temperature}°Celsius is {C}° Fahrenheit.")
  
 elif unit == "C" or unit == "c": 
- C = Temperature*1.8 + 32
- print(f"{Temperature}°Fahrenheit is {C}° Celsius.")
+ F = Temperature-32 * 1.8
+ print(f"{Temperature}° in Celsius is {F}° Fahrenheit.")
   
 else:
    print(f"Invalid Unit({unit})")
