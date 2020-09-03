3de08414c1c1dadee6dbca0bb20106b72e349ea7
JoelJohn08 <jmj6182@psu.edu>
2020-09-03 21:14:56 +0000

    laba

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-Temperature = input ("Enter Temperature:")
+Temperature = input ("Enter temperature:")
 Temperature = float(Temperature)
-unit = input ("Enter the Unit as F/f or C/c:")
-if unit == "C" or unit == "c":
- C = Temperature*1.8 +32
+unit = input (" Enter unit in F/f or C/c:")
+
+if unit == "F" or unit == "f":
+ C = (Temperature-32)*5/9
  print(f"{Temperature}° in Celsius is {C}° Fahrenheit.")
  
-elif unit == "F" or unit == "f": 
- F = Temperature-32 * 1.8
+elif unit == "C" or unit == "c": 
+ F = (Temperature*1.8)+32
  print(f"{Temperature}° in Celsius is {F}° Fahrenheit.")
   
 else:
